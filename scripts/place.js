7b22b27047a39ccbd1ace9335c3bf90af112b172
gmelo20 <ggmelo20@gmail.com>
2025-09-17 01:25:38 -0300

    modified:   place.html 	modified:   scripts/place.js 	modified:   styles/place.css

--- scripts/place.js
+++ scripts/place.js
@@ -8,25 +8,25 @@ const currentYearEl = document.getElementById("currentYear");
 const lastModifiedEl = document.getElementById("lastModified");
 
 function calculateWindChill(temp, windSpeed) {
-    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+  return 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
 }
 
 function displayWindChill(temp, windSpeed) {
-    if (temp <= 10 && windSpeed > 4.8) {
-        const chill = calculateWindChill(temp, windSpeed).toFixed(1);
-        windChillEl.textContent = chill + "°C";
-    } else {
-        windChillEl.textContent = "N/A";
-    }
+  if (temp <= 10 && windSpeed > 4.8) {
+    const chill = calculateWindChill(temp, windSpeed).toFixed(1);
+    windChillEl.textContent = chill + "°C";
+  } else {
+    windChillEl.textContent = "N/A";
+  }
 }
 
 function updateFooterInfo() {
-    const date = new Date();
-    currentYearEl.textContent = date.getFullYear();
-    lastModifiedEl.textContent = document.lastModified;
+  const date = new Date();
+  currentYearEl.textContent = date.getFullYear();
+  lastModifiedEl.textContent = document.lastModified;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    displayWindChill(temperatureC, windSpeedKmh);
-    updateFooterInfo();
+  displayWindChill(temperatureC, windSpeedKmh);
+  updateFooterInfo();
 });
